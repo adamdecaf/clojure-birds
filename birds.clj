20f4eac6d2b58a4d5f948df5db33d18664906c0d
Adam Shannon <adam.shannon@banno.com>
2014-07-23 18:00:53 -0500

    random changes.

--- birds.clj
+++ birds.clj
@@ -1,5 +1,8 @@
 ;; bird problem
 
+;; general
+(def run-tests true)
+
 ;; Types of Birds and their freqs:
 (def always-pick-freq 1.1)
 (def likely-pick-freq 0.75)
@@ -12,7 +15,7 @@
 (defn occurence-freq [history]
   (let [matching-freq (filter (fn [x] (= x :picked)) history)
         freq-count (count matching-freq)]
-  (/ freq-count (count history))))
+    (/ freq-count (count history))))
 
 (defn to-pick-or-not-to-pick [strat history]
   (let [history-freq (occurence-freq history)
@@ -38,15 +41,31 @@
 
 ;; quick tests
 (println (str "starting"))
+
+(defn do-run-tests []
+  (get-action-from-bird "always" (always-bird) test-data)
+  (get-action-from-bird "likely" (likely-bird) test-data)
+  (get-action-from-bird "sometimes" (sometimes-bird) test-data)
+  (get-action-from-bird "hardly" (hardly-bird) test-data)
+  (get-action-from-bird "never" (never-bird) test-data)
+  ;; (get-action-from-bird (random-bird) test-data))
+  )
+
 (def test-data '(:picked :picked :no-picked :no-picked))
-(get-action-from-bird "always" (always-bird) test-data)
-(get-action-from-bird "likely" (likely-bird) test-data)
-(get-action-from-bird "sometimes" (sometimes-bird) test-data)
-(get-action-from-bird "hardly" (hardly-bird) test-data)
-(get-action-from-bird "never" (never-bird) test-data)
-;; (get-action-from-bird (random-bird) test-data)
+(when run-tests (do-run-tests))
 
 ;; simulate
+(def birds [(always-bird)
+            (likely-bird) (likely-bird)
+            (sometimes-bird) (sometimes-bird) (sometimes-bird)
+            (hardly-bird)
+            (never-bird) (never-bird)])
+
+;; fold through, acc'iming history events to pass through into the next call.
+;; recur it several times to keep simulating over and over
+;; (defn simulate
+;;   ([] (simulate []))
+;;   ([history] ...)
 
 ;; exit
 (println (str "done"))
